a8251234fbfb072de63cdc04759e2d9cc3265ec7
ilpyoyang <ilpyoyang@gmail.com>
2025-03-13 11:45:58 +0900

    class 생성

---
+++ lib/shopping_mall.dart
@@ -0,0 +1,30 @@
+import 'package:console_shoppingmaill/product.dart';
+
+class ShoppingMall {
+  // 초기 데이터
+  List<Product> stores = [
+    Product(name: '셔츠', price: 45000, count: 1),
+    Product(name: '원피스', price: 30000, count: 2),
+    Product(name: '반팔티', price: 500000, count: 3),
+    Product(name: '반바지', price: 400000, count: 4),
+    Product(name: '양말', price: 300000, count: 0),
+  ];
+
+  int totalPrice = 0;
+  
+  // ShoppingMall({required this.stores, required this.totalPrice});
+
+  void showProducts() {
+    for(var product in stores) {
+      print('${product.name} / ${product.price}원');
+    }
+  }
+
+  void addToCart() {
+    
+  }
+
+  void showTotal() {
+    
+  }
+}
